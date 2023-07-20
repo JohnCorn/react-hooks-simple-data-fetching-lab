8ee137919c69ae3f3bea963646c42484d8e47282
John Corn <jcornacchioli@gmail.com>
2023-07-20 10:31:48 -0400

    lab complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,31 @@
-// create your App component here
+import React, {useEffect, useState} from "react";
+
+function App()
+{
+
+    const [pic, setPic] = useState(null);
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response) => response.json())
+        .then((data) => {
+            console.log("data: ", data)
+            setPic(data)
+        });
+    }, []);
+
+    if(!pic){
+        return(<p>Loading...</p>);
+    }
+
+    return(
+        <div>
+            <img
+            src={pic.message}
+            alt="A Random Dog"
+            />
+        </div>
+    );
+}
+
+export default App;
